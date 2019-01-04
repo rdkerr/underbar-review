820c5d0de0c5b71fa266a4bf53b5f8bd232bb8e8
rdkerr <rogerdkerr@gmail.com>
2019-01-03 21:58:21 -0700

    Finished every

--- spec/part2.js
+++ spec/part2.js
@@ -93,7 +93,8 @@
       it('fails for a collection of all-falsy values', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        expect(_.every([false, '', null], _.identity)).to.be.false;
+        // throw new Error('This test is missing.');
       });
 
       it('fails for a collection containing mixed falsy and truthy values', function() {
--- src/underbar.js
+++ src/underbar.js
@@ -192,7 +192,15 @@
 
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
-    // TIP: Try re-using reduce() here.
+    if(iterator === undefined) {
+      iterator = _.identity;
+    };
+    return _.reduce(collection, function(prevAns, item) {
+      if (prevAns && iterator(item)) {
+        return true;
+      }
+      return false;
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
