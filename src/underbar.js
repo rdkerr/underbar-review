44b54681d42ee6ddf9fcb359d3a24404db566e2b
rdkerr <rogerdkerr@gmail.com>
2019-01-03 23:20:13 -0700

    Finished delay

--- src/underbar.js
+++ src/underbar.js
@@ -314,6 +314,10 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var args = Array.from(arguments).slice(2);
+    return setTimeout(function() {
+      func.apply(this, args);
+    }, wait);
   };
 
 
